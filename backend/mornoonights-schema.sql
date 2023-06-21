fb39e836c79f7c33118b412cdcfc9facce854e5b
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-20 20:19:18 -0700

    tables added

--- backend/mornoonights-schema.sql
+++ backend/mornoonights-schema.sql
@@ -1,11 +1,21 @@
 CREATE TABLE users (
     username VARCHAR(30) PRIMARY KEY,
-    first_name VARCHAR(25) NOT NULL,
-    last_name VARCHAR(25) NOT NULL,
+    first_name VARCHAR(25) NOT NULL CHECK (first_name = LOWER(first_name)),
+    last_name VARCHAR(25) NOT NULL CHECK (last_name = LOWER(last_name)),
     password TEXT NOT NULL,
-    email TEXT NOT NULL CHECK (position('@' IN email) > 1)
+    email TEXT NOT NULL CHECK (position('@' IN email) > 1),
+    created_at TIMESTAMP DEFAULT NOW(),
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE recents (
+    news_id TEXT NOT NULL,
+    username VARCHAR(30) REFERENCES users ON DELETE CASCADE,
+    PRIMARY KEY (news_id, username)
+);
 
+CREATE TABLE favorites (
+    news_id TEXT NOT NULL,
+    username VARCHAR(30) REFERENCES users ON DELETE CASCADE,
+    PRIMARY KEY (news_id, username)
 );
