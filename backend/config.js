8859b8127084fc160447418acde62d4bf6d79dbc
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-21 20:38:37 -0700

    configuration for app

---
+++ backend/config.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/** Shared config for application; can be required many places. */
+
+require("dotenv").config();
+
+const SECRET_KEY = process.env.SECRET_KEY || "MorNooNigthsNewsAppSecretDev";
+
+const PORT = +process.env.PORT || 3001;
+
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+      ? "mornoonights_test"
+      : process.env.DATABASE_URL || "mornoonights";
+}
+
+// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+console.log("MorNooNights Config--");
+console.log("SECRET_KEY::", SECRET_KEY);
+console.log("PORT::", PORT.toString());
+console.log("BCRYPT_WORK_FACTOR::", BCRYPT_WORK_FACTOR);
+console.log("Database::", getDatabaseUri());
+
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri,
+};
