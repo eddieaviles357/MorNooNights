056539ff15e86dd5091a6ddb6432972c3e723129
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-21 20:38:22 -0700

    bones for the backend

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new Error("NOT FOUND"));
+  });
+
+/** Generic error handler. Anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
