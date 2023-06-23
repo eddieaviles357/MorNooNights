24137106f640538a9da4b1cd7af12f24803cb10c
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-23 10:42:00 -0700

    console logs are more clear

--- backend/config.js
+++ backend/config.js
@@ -18,11 +18,14 @@ function getDatabaseUri() {
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-console.log("MorNooNights Config--");
-console.log("SECRET_KEY::", SECRET_KEY);
-console.log("PORT::", PORT.toString());
-console.log("BCRYPT_WORK_FACTOR::", BCRYPT_WORK_FACTOR);
-console.log("Database::", getDatabaseUri());
+console.log("*********************************************")
+console.log("*********   MorNooNights Config   ***********\n")
+console.log("1:SECRET_KEY         :  ", SECRET_KEY);
+console.log("2:PORT               :  ", PORT.toString());
+console.log("3:BCRYPT_WORK_FACTOR :  ", BCRYPT_WORK_FACTOR);
+console.log("4:Database           :  ", getDatabaseUri());
+console.log("\n*********   MorNooNights Config   ***********")
+console.log("*********************************************\n")
 
 module.exports = {
   SECRET_KEY,
