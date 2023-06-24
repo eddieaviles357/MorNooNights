30403f1b859b236cb4aefa8f9cd8269cff4a918d
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-24 15:42:44 -0700

    authenticates user and returns a jwt for future request

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const jsonschema = require("jsonschema");
 const User = require("../models/User");
 const userRegisterSchema = require("../schemas/userRegisterSchema");
+const userAuthSchema = require("../schemas/useAuthSchema");
 const { BadRequestError } = require("../MornoonightsError");
 const createToken = require("../helpers/tokens");
 
@@ -28,4 +29,25 @@ router.post("/register", async function (req, res, next) {
     }
   });
 
+  /** POST /auth/token:  { username, password } => { token }
+ *
+ * Returns JWT token which can be used to authenticate further requests. 
+ */
+
+router.post("/token", async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, userAuthSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+  
+      const { username, password } = req.body;
+      const user = await User.authenticate(username, password);
+      const token = createToken(user);
+      return res.json({ token });
+    } catch (err) {
+      return next(err);
+    }
+  });
 module.exports = router;
