f940fd1f279739e639cfa79cf18e70b7c2e19ebf
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-29 21:40:22 -0700

    if no routes match throw a MonoonightsError object

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,22 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const { NotFoundError } = require("./MornoonightsError");
+const { authenticateJWT } = require("./middleware/auth");
 const newsRoutes = require("./routes/news");
 const authRoutes = require("./routes/auth");
 
 app.use(cors());
 app.use(express.json());
+app.use(authenticateJWT);
+
 
 app.use("/news", newsRoutes);
 app.use("/auth", authRoutes);
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
-    return next(new Error("NOT FOUND"));
+    return next(new NotFoundError());
   });
 
 /** Generic error handler. Anything unhandled goes here. */
