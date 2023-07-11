00f5281910a5347c06d71c3ef09b4d76134bc77c
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-07-10 19:19:29 -0700

    variable to hold jwtoken

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import Navigation from "./components/routes-nav/Navigation"
 import Routes from "./components/routes-nav/Routes"
+import jwt from "jsonwebtoken";
+
+// Used for storing token in localStorage for "remember me" re-login
+export const TOKEN_STORAGE_ID = "mornoonightsnews-token";
 
 function App() {
   return (
