aad8b5a4a67aee8115f65c23f5e6e57dabd55262
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-07-11 20:31:42 -0700

    userContext is used for current user based on that information if no valid user displays login or singup component if current user is valid displays user name

--- frontend/src/components/routes-nav/Navigation.js
+++ frontend/src/components/routes-nav/Navigation.js
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { NavLink, Link } from "react-router-dom";
 import "./Navigation.css";
+import UserContext from "../auth/UserContext"; 
 
-export default function Navigation() {
+export default function Navigation({ logout }) {
+  
+  const {currentUser} = useContext(UserContext);
+  console.log("NAVIGATION CURR_USER::", currentUser);
+  // displays when user is logged in
+  function loggedInNav() {
     return (
-        <nav className="Nav">
-            <Link to='/'>MorNooNightsNews</Link>
-            <ul>
-                <li><NavLink to='/profile'>Profile</NavLink></li>
-                <li><NavLink to='/signin'>Sign in</NavLink></li>
-                <li><NavLink to='/signout'>Sign out</NavLink></li>
-            </ul>
-        </nav>
+        <ul>
+          <li>
+            <Link to="/" onClick={logout}>
+              Log out {currentUser.first_name || currentUser.username}
+            </Link>
+          </li>
+        </ul>
     );
+  };
+  // displays when user is logged out or is a new user
+  function loggedOutNav() {
+    return (
+        <ul>
+            <li><NavLink to='/login'>Login</NavLink></li>
+            <li><NavLink to='/signup'>Sign Up</NavLink></li>
+        </ul>
+    );
+  };
+
+  return (
+    <nav className="Nav">
+      <Link className="navbar-brand" to="/">
+        MorNooNightsNews
+      </Link>
+      {currentUser ? loggedInNav() : loggedOutNav()}
+    </nav>
+  );
 };
 
 
