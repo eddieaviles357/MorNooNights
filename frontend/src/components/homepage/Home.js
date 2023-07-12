eec62a1571b39ce30d9415d95ed7449138011e74
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-07-11 21:38:36 -0700

    class added to home page

--- frontend/src/components/homepage/Home.js
+++ frontend/src/components/homepage/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
     return (
-        <div>
+        <div className="Home">
             Home page
         </div>
     )
