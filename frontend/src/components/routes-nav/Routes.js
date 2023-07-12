ccf4490c4598790c07c5ee4792ca30fc08b37d77
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-07-11 20:32:59 -0700

    name change to AppRoutes profile was deleted, login and signup are currently only available

--- frontend/src/components/routes-nav/Routes.js
+++ frontend/src/components/routes-nav/Routes.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../homepage/Home";
-import ProfileForm from "../profiles/ProfileForm";
+import LoginForm from "../auth/LoginForm";
+import SignupForm from "../auth/SignupForm";
 
-export default function Navigation(params) {
+export default function AppRoutes({ login, signup}) {
   return(
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<ProfileForm />} />
+        <Route path="/login" element={<LoginForm login={login} />}></Route>
+        <Route path="/signup" element={<SignupForm signup={signup} />}></Route>
     </Routes>
   )
 }
