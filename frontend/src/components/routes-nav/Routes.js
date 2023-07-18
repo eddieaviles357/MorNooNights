7be1dc7871984e162f2618c24a960ae9860be199
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-07-17 21:13:06 -0700

    private routes added

--- frontend/src/components/routes-nav/Routes.js
+++ frontend/src/components/routes-nav/Routes.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 import Home from "../homepage/Home";
 import LoginForm from "../auth/LoginForm";
 import SignupForm from "../auth/SignupForm";
+import TopNews from "../news/TopNews";
 
 export default function AppRoutes({ login, signup}) {
+
   return(
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginForm login={login} />}></Route>
-        <Route path="/signup" element={<SignupForm signup={signup} />}></Route>
+        <Route path="/" index element={<Home />} />
+        <Route path="/login" element={<LoginForm login={login} />} />
+        <Route path="/signup" element={<SignupForm signup={signup} />} />
+        {/* Private route -- access to current users or admin */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/news" element={<TopNews />} />
+        </Route>
+
     </Routes>
   )
 }
