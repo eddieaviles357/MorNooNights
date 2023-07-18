b1b105a92ec72264ed345b81a497dcd3cba369c8
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-07-17 21:12:38 -0700

    updated route to version 6 syntax

--- frontend/src/components/routes-nav/PrivateRoute.js
+++ frontend/src/components/routes-nav/PrivateRoute.js
@@ -1,33 +1,26 @@
-import React, { useContext } from "react";
-import { Route, redirect } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import UserContext from "../auth/UserContext";
 
 /** "Higher-Order Component" for private routes.
  *
  * In routing component. 
- * This component will check if there is a valid current user 
+ * This component will check if isAllowed is true
  * and only continues to the route if so. 
- * If no user is present, redirects to login form.
+ * If isAllowed is true, redirects using redirectPath prop.
+ * Default redirect is login form.
  */
 
-function PrivateRoute({path, children }) {
+function PrivateRoute({ redirectPath = "/login", children }) {
   const { currentUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-  console.debug(
-      "PRIVATE_ROUTE",
-      "path::", path,
-      "currentUser::", currentUser,
-  );
-  // redirect if no current user is present
-  if (!currentUser) {
-    return redirect("/login");
-  }
-
-  return (
-      <Route path={path}>
-        {children}
-      </Route>
-  );
+  // redirect no current user exist
+  useEffect(() => {
+    if(!currentUser) navigate(redirectPath)
+  }, [currentUser, navigate, redirectPath])
+  // can be used as layout component or a wrapper component
+  return children ? children : <Outlet />;
 }
 
 export default PrivateRoute;
