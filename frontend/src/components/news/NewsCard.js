0e86cea30dbc2e87b0f74fb66973fff994fd5ea1
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-07-19 21:21:39 -0700

    clicking on a news article will trigger an update to local storage hook

--- frontend/src/components/news/NewsCard.js
+++ frontend/src/components/news/NewsCard.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../auth/UserContext";
 
 import "./NewsCard.css";
 
 
 /** Show limited information about a news article */
 
-export default function NewsCard({ 
-      uuid, title, description, keywords,
-      snippet, url, language, source,
-      locale, imageURL, publishedAt,
-      categories = [] }) {
+export default function NewsCard({ uuid, title, snippet, url, imageURL, publishedAt}) {
+  const { visitedNews, setVisitedNews} = useContext(UserContext);
   console.log("NewsCard Rendered");
+
+  const handleVisitedClick = () => {
+    // if no data is available set to empty array
+    const visited = JSON.parse(visitedNews) || [];
+    
+    // allows only 5 entries
+    if(visited.length >= 5) visited.shift(); // remove 1st entry
+
+    // stringify visited news and update values
+    setVisitedNews( _ => JSON.stringify(
+      [...visited, { uuid, vistedAt: new Date().toGMTString() }] )
+    )
+  }
   return (
     <div className="NewsCard">
       <div className="img-container">
@@ -20,7 +31,8 @@ export default function NewsCard({
       <div className="Body card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{snippet}</p>
-        <Link className="Btn-tertiary btn" to={url}>Read More</Link>
+        <p className="card-text fst-italic">Published: {new Date(publishedAt).toUTCString()}</p>
+        <Link onClick={handleVisitedClick} className="Btn-tertiary btn" to={url} target="_blank">Read More</Link>
       </div>
     </div>
   );
