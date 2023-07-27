62b51c23c329d31f4b17b03651378d8c5ae07ec8
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-07-26 22:09:07 -0700

    when making a call to the api for recently viewed news will either have data or be set to an empty array, when mapping through data we will not be using categories

--- frontend/src/components/news/TopNews.js
+++ frontend/src/components/news/TopNews.js
@@ -41,13 +41,12 @@ export default function TopNews() {
     search();
   }, []);
 
+  // fetch recents data from db
   useEffect(() => {
     async function recents(username) {
       try {
-        const {recents} = await MorNooNightsNewsAPI.getRecents(currentUser.username);
-        console.log('--RECENTS-API-CALL--', recents);
+        const {recents} = await MorNooNightsNewsAPI.getRecents( currentUser.username ) || [];
         setVisitedNews( JSON.stringify(recents) );
-
       } catch (err) {
         setErrors(Array.from(err || err.message))
       };
@@ -60,9 +59,9 @@ export default function TopNews() {
     console.error('API::ERROR::',errors);
     return <div className="d-flex justify-content-center ">{errors.map(e => (<div>Sorry an Error Ocurred</div>) )}</div>
   };
+
   // Loader
   if (!topNews) return <div className="p-4">Loading...</div>;
-  console.log('TOP::NEWS::API::DATA', topNews)
 
   return (
       <div className="d-flex flex-column align-items-center">
@@ -70,12 +69,26 @@ export default function TopNews() {
             ? (
                 <div className="d-flex flex-row justify-content-center align-items-center flex-wrap my-5">
                   {topNews.data.map( 
-                    newsObj => (
+                    ({ 
+                      uuid, 
+                      title, 
+                      description, 
+                      keywords, 
+                      snippet, 
+                      url, 
+                      language, 
+                      locale, 
+                      image_url, 
+                      source, 
+                      published_at }) => {
+                        
+                      const nObj = { uuid, title, description, keywords, snippet, url, language, locale, image_url, source, published_at };
+                      return (
                       <NewsCard 
-                        key={newsObj.uuid}
-                        newsObj={newsObj}
+                        key={uuid}
+                        newsObj={nObj}
                         updateRecentlyVisited={updateRecentlyVisited} />
-                        )
+                        )}
                     )
                   }
                 </div>
