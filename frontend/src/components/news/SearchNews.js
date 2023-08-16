e54a14a5e4d95dc9b82cf8e45c71813af6293d5a
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-08-15 20:35:58 -0700

    search news component added basic file

---
+++ frontend/src/components/news/SearchNews.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MorNooNightsNewsAPI from "../../api/api";
+import NewsCard from "./NewsCard";
+import UserContext from "../auth/UserContext";
+
+export default function SearchNews() {
+  return (
+    <div>
+      Search News
+    </div>
+  )
+}
