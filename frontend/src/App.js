0113139ea721d1abf48e0d4741e8aabcf65840d3
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-08-22 20:50:36 -0700

    Main App will handle all errors and fetching users recently visited news articles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,34 +18,9 @@ function App() {
   const [news, setNews] = useState(null);
   const [visitedNews, setVisitedNews] = useLocalStorage("recents");
   const [topNews, setTopNews] = useState([]);
-  const [currPage, setCurrPage] = useState(1);
-
-  // console.debug("currentUser", currentUser, "token", token);
-
-  // is mounted is used for React development since it calls useEffect twice
-  async function search(mounted = false, name = '', page = 1) {
-    if(!mounted) setCurrPage(2);
-    try {
-      const news = await MorNooNightsNewsAPI.getTopNews(name, page);
-      // used for testing so we won't exhaust our api calls
-      // const news = fakeData;
-      const { data: { data } } = news;
-
-      console.log('extracted_data_',data);
-      !mounted ? 
-        setTopNews( data ) :
-        setTopNews( prev => [ ...prev, ...data ]);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  async function loadMoreTopNews() {
-    setCurrPage(_=>currPage+1);
-    search(true, '', currPage);
-  }
-
-  useEffect(function loadUserInfo() {
+  const [errors, setErrors] = useState(null);
+  
+  useEffect(() => {
     console.debug("App useEffect loadUserInfo", "token=", token);
 
     async function getCurrentUser() {
@@ -56,7 +31,6 @@ function App() {
           MorNooNightsNewsAPI.token = token;
           let currentUser = await MorNooNightsNewsAPI.getCurrentUser(username);
           setCurrentUser(currentUser);
-          search();
         } catch (err) {
           console.error("App loadUserInfo: problem loading", err);
           setCurrentUser(null);
@@ -64,10 +38,25 @@ function App() {
       };
       setInfoLoaded(true);
     }
-
     setInfoLoaded(false);
     getCurrentUser();
   }, [token]);
+
+  useEffect(() => {
+    async function fetchRecents() {
+      if(token && currentUser) {
+        try {
+          const {username} = currentUser;
+          const {recents} = await MorNooNightsNewsAPI.getRecents( username ) || [];
+          setVisitedNews( JSON.stringify(recents) );
+        } catch (err) {
+          setErrors(Array.from(err || err.message));
+          console.log(err);
+        };
+      }
+    };
+    fetchRecents();
+  }, [currentUser]);
 
   // Handles site logout.
   async function logout() {
@@ -119,7 +108,6 @@ function App() {
     // if no data is available set to empty array
     const visited = JSON.parse(visitedNews) || [];
     const updatedNews = {...news, visited_at: new Date().toDateString() }
-    console.log('%cUPDATED::NEWS::', 'color: lightblue;', updatedNews);
     // allows only 5 entries
     if(visited.length >= 5) visited.shift(); // remove 1st entry
     visited.push(updatedNews)
@@ -141,9 +129,10 @@ function App() {
         updateRecentlyVisited,
         news,
         setNews,
-        loadMoreTopNews,
         topNews,
-        setTopNews
+        setTopNews,
+        errors,
+        setErrors,
         }}>
         <Navigation logout={logout}/>
         <AppRoutes login={login} signup={signup}/>
@@ -154,48 +143,3 @@ function App() {
 }
 
 export default App;
-
-const fakeData = {data: {
-  meta: {found: 738236, returned: 3, limit: 3, page: 1},
-  data: [
-    {
-  uuid: '8fc8718e-1b93-4baa-8013-058821959122',
-  title: 'Man found dead following shed fire in Palmer',
-  description: "The shed was ablaze when firefighters arrived, with flames “threatening to spread to the residence and nearby woods,” officials said.",
-  keywords: "",
-  snippet: "“Our hearts go out to this man’s family and loved ones,” Palmer Fire Chief William J. Bernat Jr. said in the statement. “On behalf of the Palmer Fire De...",
-  url: "https://www.bostonglobe.com/2023/07/17/metro/man-found-dead-following-shed-fire-palmer/?camp=bg:brief:rss:feedly&rss_id=feedly_rss_brief",  
-  language: "en",
-  categories: ['general'],
-  locale: "us",
-  image_url: "https://www.bostonglobe.com/pf/resources/images/logo-bg.jpg?d=420",
-  source: "bostonglobe.com",
-  published_at: "2023-07-18T01:51:32.000000Z",
-}, {
-  uuid: "ab6b9480-a610-4a71-a7e4-6023b525c105",
-  title: "Emily Blunt Reveals Cillian Murphy’s Strict Oppenheimer Diet",
-  description: "The shed was ablaze when firefighters arrived, with flames “threatening to spread to the residence and nearby woods,” officials said.",
-  keywords: "",
-  snippet: "Watch : Matt Damon & Emily Blunt Reveal Their Daughters' CLOSE Bond\n\nCillian Murphy's role in Oppenheimer pushed him to extreme lengths.\n\nIn fact, his co-star E...",
-  url: "https://www.eonline.com/news/1380473/emily-blunt-reveals-cillian-murphy's-strict-lessigreateroppenheimerlessigreater-diet?cmpid=rss-syndicate-genericrss-us-top_stories",  
-  language: "en",
-  categories: ['entertainment', 'general'],
-  locale: "us",
-  image_url: "https://akns-images.eonline.com/eol_images/Entire_Site/2023617/rs_1200x1200-230717170936-1200-Emily-Blunt-Cillian-Murphy.cm.71723.jpg?fit=around%7C1080:1080&output-quality=90&crop=1080:1080;center,top",
-  source: "eonline.com",
-  published_at: "2023-07-18T01:49:03.000000Z",
-}, {
-  uuid: 'cad54b33-860d-4bbd-96e6-e5551ec4fac2',
-  title: "Singapore’s passport is now the most powerful in the world. Here's how other countries ranked",
-  description: "Singapore has overtaken Japan to boast the most powerful passport in the world, the Henley Passport Index showed.",
-  keywords: "U.S. Economy, Asia Economy, South Korea, Singapore, Japan, business news",
-  snippet: "Singapore has overtaken Japan to boast of the world's most powerful passport, the Henley Passport Index showed.\n\nWhat it means is that the Singapore passport al...",
-  url: "https://www.cnbc.com/2023/07/19/henley-passport-index-2023-singapore-germany-italy-spain-rank-top-.html",  
-  language: "en",
-  categories: ['general', 'business'],
-  locale: "us",
-  image_url: "https://image.cnbcfm.com/api/v1/image/107273388-1689733279342-gettyimages-821304076-singaporesunset2.jpeg?v=1689733412&w=1920&h=1080",
-  source: "cnbc.com",
-  published_at: "2023-07-19T02:24:26.000000Z",
-}
-]}}
