127295cad49e5f52e2dadc9a12fbf6dffc81521a
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-08-23 20:51:41 -0700

    updated comments

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -29,15 +29,10 @@ class MorNooNightsNewsAPI {
 
   // API routes
 
+  /******* User specific API endpoints ********/
   static async getCurrentUser(username) {
     let res = await this.request(`users/${username}`);
     return res.user;
-  };
-
-  static async getTopNews(name = '', page = 1) {
-    let res = await this.request(`news/top/${page}`);
-    console.log('%cAPI::FRONTEND::GET_TOP_NEWS', "color: yellow; font-size: 20px;", res);
-    return res;
   };
 
   static async getRecents(username) {
@@ -51,27 +46,33 @@ class MorNooNightsNewsAPI {
     const res = await this.request(`users/${username}/recents`, data , "post");
   }
 
+  /****** News specific API endpoints *******/
+  static async getTopNews(name = '', page = 1) {
+    let res = await this.request(`news/top/${page}`);
+    console.log('%cAPI::FRONTEND::NEWS::GET_TOP_NEWS', "color: yellow; font-size: 20px;", res);
+    return res;
+  };
+
   static async searchNews(val, page = 1) {
     const res = await this.request(`news/search/${val}/${page}`);
-    console.log("%cAPI::FRONTEND::SEARCH_NEWS::", "color: yellow; font-size: 20px;", res);
+    console.log("%cAPI::FRONTEND::NEWS::SEARCH_NEWS::", "color: yellow; font-size: 20px;", res);
     return res;
   }
 
   static async getNewsByCategory(category, page = 1) {
     const res = await this.request(`news/category/${category}/${page}`);
-    console.log("%cAPI::FRONTEND::GET_NEWS_BY_CATEGORY", "color: yellow; font-size: 20px;", res);
+    console.log("%cAPI::FRONTEND::NEWS::GET_NEWS_BY_CATEGORY", "color: yellow; font-size: 20px;", res);
     return res;
   }
 
   // Get token for login.
-
+  /**** AUTH specific endpoints *******/
   static async login(data) {
     let res = await this.request(`auth/token`, data, "post");
     return res.token;
   };
 
   // Signup for site.
-
   static async signup(data) {
     let res = await this.request(`auth/register`, data, "post");
     return res.token;
