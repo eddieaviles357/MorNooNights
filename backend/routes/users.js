b9bc5448ea759704606918eb97c7c4cf5bb96048
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-08-23 20:44:00 -0700

    transferred routes from news to users, recents will now be handles within users routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -24,5 +24,52 @@ router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, nex
   }
 });
 
+/** Update recents news for User.
+* 
+* Returns undefined
+*  
+* Must be correct user or admin
+**/
+router.post("/:username/recents", ensureCorrectUserOrAdmin, async (req, res, next) => {
+  try {
+      const { username } = req.params; 
+      let { recents } = req.body;
+      
+      // console.log('\nPOST::NEWS/{username}/RECENTS::');
+      await User.setRecents(username, recents);
+      return res.json({ success: true });
+  } catch (err) {
+      return next(err);
+  }
+});
 
+/** Gets recents news from User.
+* 
+* Returns recent news obj
+* {
+*	 recents: [
+*		{ uuid, title, description, keywords,
+*		  snippet, url, image_url, language,
+*		  published_at, source, categories: [], visitedAt }
+*	 ]
+* }
+*  
+* Returns an Empty array [] if no recent news exist.
+*
+* If an error occurs in retrieving recent news an error obj will return { error: msg }
+*
+* Must be correct user or admin
+**/
+router.get("/:username/recents", ensureCorrectUserOrAdmin, async (req, res, next) => {
+  try {
+      const { username } = res.locals.user; 
+      // get recently visited news -> [{ uuid, visitedAt}]
+      let recents = await User.getRecents(username);
+
+      // console.log('\nGET::NEWS/{username}/RECENTS::', recents);
+      return res.json({ recents });
+  } catch (err) {
+      return next(err);
+  }
+});
 module.exports = router;
