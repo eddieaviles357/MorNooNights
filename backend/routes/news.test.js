a585ee865b0b399c90244182083aa610f5ad86bd
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-08-29 19:42:00 -0700

    test for search route, also added test to check against invalid user on all tested routes

--- backend/routes/news.test.js
+++ backend/routes/news.test.js
@@ -35,10 +35,19 @@ describe("GET /news/top/:page", function () {
   });
 });
 
+describe("GET /news/top/:page", function () {
+  test("fails with no valid user", async function () {
+    const resp = await request(app)
+        .get("/news/top/1")
+    expect(resp.statusCode).toEqual(401);
+    expect(resp.body).toEqual({ error: { message: 'Unauthorized', status: 401 } });
+  });
+});
+
 
 /************************************** POST "/category/:categories/:page" */
 describe("GET /category/:categories/:page", function () {
-  test("works and returns a news based on category given", async function () {
+  test("works and returns news given a category", async function () {
     const resp = await request(app)
         .get("/news/category/tech/1")
         .set("authorization", `Bearer ${u1Token}`);
@@ -50,5 +59,42 @@ describe("GET /category/:categories/:page", function () {
     // checks if all the data in "categories" has value of "tech"
     expect(data.every( n => n['categories'].some( c => c === 'tech' ) ))
     .toBe(true);
+  });
+});
+
+describe("GET /category/:categories/:page", function () {
+  test("fails with no valid user", async function () {
+    const resp = await request(app)
+        .get("/news/category/tech/1");
+
+    expect(resp.statusCode).toEqual(401);
+    expect(resp.body).toEqual({ error: { message: 'Unauthorized', status: 401 } });
+  });
+});
+
+/************************************** POST "/search/:value/:page" */
+describe("GET /search/:value/:page", function () {
+  test("works and returns news using search value in params", async function () {
+    const searchKeyword = "hot";
+    const resp = await request(app)
+        .get(`/news/search/${searchKeyword}/1`)
+        .set("authorization", `Bearer ${u1Token}`);
+    const { data: { data } } = resp.body;
+    expect(resp.statusCode).toEqual(200);
+    expect(data).toEqual(expect.any(Array));
+    expect(data).toEqual(expect.any(Object));
+    expect(data.length).toEqual(3);
+    expect(data.every( n => n['title'].toLowerCase().includes(searchKeyword) ) )
+    .toBe(true);
+  });
+});
+
+describe("GET /search/:value/:page", function () {
+  test("fails with no valid user", async function () {
+    const resp = await request(app)
+        .get("/news/search/hot/1");
+
+    expect(resp.statusCode).toEqual(401);
+    expect(resp.body).toEqual({ error: { message: 'Unauthorized', status: 401 } });
   });
 });
