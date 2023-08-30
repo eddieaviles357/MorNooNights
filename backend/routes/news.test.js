e35fea7ede793ba5f1c9bcf27595f56c80730160
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-08-29 20:44:03 -0700

    test for uuid

--- backend/routes/news.test.js
+++ backend/routes/news.test.js
@@ -9,18 +9,20 @@ const {
   commonBeforeEach,
   commonAfterEach,
   commonAfterAll,
+  uuids,
   u1Token, 
   u2Token,
   news,
   categoryNews
 } = require("./_testCommon");
+const { FAKE_DATA } = require("../mockData");
 
 beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
-/************************************** POST /news/top/:page */
+/************************************** GET /news/top/:page */
 
 describe("GET /news/top/:page", function () {
   test("works and returns news", async function () {
@@ -45,7 +47,7 @@ describe("GET /news/top/:page", function () {
 });
 
 
-/************************************** POST "/category/:categories/:page" */
+/************************************** GET "/category/:categories/:page" */
 describe("GET /category/:categories/:page", function () {
   test("works and returns news given a category", async function () {
     const resp = await request(app)
@@ -72,7 +74,7 @@ describe("GET /category/:categories/:page", function () {
   });
 });
 
-/************************************** POST "/search/:value/:page" */
+/************************************** GET "/search/:value/:page" */
 describe("GET /search/:value/:page", function () {
   test("works and returns news using search value in params", async function () {
     const searchKeyword = "hot";
@@ -98,3 +100,26 @@ describe("GET /search/:value/:page", function () {
     expect(resp.body).toEqual({ error: { message: 'Unauthorized', status: 401 } });
   });
 });
+
+/************************************** GET "uuid/:id" */
+describe("GET /uuid/:id", function () {
+  test("fetches news by uuid", async function () {
+    const resp = await request(app)
+        .get(`/news/uuid/${uuids[0]}`)
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body.data.uuid).toEqual(uuids[0]);
+  });
+});
+
+describe("GET uuid/:id", function () {
+  test("fails with no valid user", async function () {
+    const resp = await request(app)
+        .get(`/news/uuid/${uuids[0]}`);
+
+    expect(resp.statusCode).toEqual(401);
+    expect(resp.body).toEqual({ error: { message: 'Unauthorized', status: 401 } });
+  });
+});
+
+
