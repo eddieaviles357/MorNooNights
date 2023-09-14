bb87413e8195c64ebf823a1476f58751b933e9ae
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-09-13 19:36:36 -0700

    updated test

--- backend/frontend/src/__test__/App.test.js
+++ backend/frontend/src/__test__/App.test.js
@@ -1,8 +1,6 @@
 import { render } from '@testing-library/react';
 import App from '../App';
-import axios from 'axios';
 
-// jest.mock(axios);
 
 test('renders without crashing', () => {
   render(<App />);
@@ -13,11 +11,8 @@ it('matches snapshot', () => {
   expect(app.asFragment()).toMatchSnapshot();
 });
 
-it('should display title h1', () => {
-  const {asFragment, getByText} = render(<App />)
-  console.log(asFragment)
-  expect(getByText('MorNooNightsNews')).toBeInTheDocument()
-  expect(asFragment()).toMatchInlineSnapshot(`
-  <h1 class="mb-4 text-white font-weight-bold">MorNooNightsNews</h1>
-`)
+it('should display all mornoonights elements', () => {
+  const {queryAllByText} = render(<App />);
+  const title = queryAllByText(/mornoonightsnews/i);
+  expect(title.length).toBe(2)
 });
