27cde0f58d7d9075195b886bd7836971df87bece
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-09-13 20:13:35 -0700

    updated test

--- backend/frontend/src/__test__/App.test.js
+++ backend/frontend/src/__test__/App.test.js
@@ -1,18 +1,25 @@
 import { render } from '@testing-library/react';
 import App from '../App';
+import { MemoryRouter } from "react-router";
 
-
-test('renders without crashing', () => {
-  render(<App />);
-});
-
-it('matches snapshot', () => {
-  const app = render(<App />);
-  expect(app.asFragment()).toMatchSnapshot();
-});
-
-it('should display all mornoonights elements', () => {
-  const {queryAllByText} = render(<App />);
-  const title = queryAllByText(/mornoonightsnews/i);
-  expect(title.length).toBe(2)
-});
+describe("App component", () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+  
+  it('matches snapshot', () => {
+    const app = render(<App />);
+    expect(app.asFragment()).toMatchSnapshot();
+  });
+  
+  it('should display all mornoonights elements', () => {
+    const {queryAllByText} = render(<App />);
+    const title = queryAllByText(/mornoonightsnews/i);
+    expect(title.length).toBe(2)
+  });
+  
+  it('should display log in or sign in nav components', () => {
+    const { asFragment } = render(<MemoryRouter /> );
+    expect(asFragment()).toMatchSnapshot();
+  })
+})
