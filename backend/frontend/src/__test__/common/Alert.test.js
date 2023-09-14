e45fea34df900fb450237e5ac4041bea1af81252
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-09-13 20:52:48 -0700

    cleanup

--- backend/frontend/src/__test__/common/Alert.test.js
+++ backend/frontend/src/__test__/common/Alert.test.js
@@ -1,28 +1,34 @@
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import Alert from '../../components/common/Alert';
 
-test('renders without crashing', () => {
-  render(<Alert />);
-});
+afterAll(() => {
+  cleanup();
+})
 
-it('matches snapshot', () => {
-  const { asFragment } = render(<Alert />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-it('matches snapshot for error message', () => {
-  const { asFragment } = render(<Alert messages={["error"]}/>);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-it("matches snapshot for success mesage", function() {
-  let messages = ["Green"];
-  const { asFragment } = render(<Alert type="success" messages={messages} />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-it("messages are displayed", function() {
-  let messages = ["error"];
-  const alert = render(<Alert type="success" messages={messages} />);
-  expect(alert.queryByText('error')).toBeInTheDocument();
-});
+describe("Alert component", () => {
+  it('renders without crashing', () => {
+    render(<Alert />);
+  });
+  
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Alert />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  it('matches snapshot for error message', () => {
+    const { asFragment } = render(<Alert messages={["error"]}/>);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  it("matches snapshot for success mesage", function() {
+    let messages = ["Green"];
+    const { asFragment } = render(<Alert type="success" messages={messages} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  it("messages are displayed", function() {
+    let messages = ["error"];
+    const alert = render(<Alert type="success" messages={messages} />);
+    expect(alert.queryByText('error')).toBeInTheDocument();
+  });
+})
